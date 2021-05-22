1511f4c7dcfad68bb4c7d7062d8b9b98b0f07c4e
Jonathan98-dev <email.jonathannuel@gmail.com>
2021-05-22 12:07:20 +0200

    slider functional

--- jum-website-project/src/components/Gallery/Gallery.js
+++ jum-website-project/src/components/Gallery/Gallery.js
@@ -1,27 +1,43 @@
 import React from "react";
+import useWindowDimensions from "../WindowDimensions/getWindowDimensions";
 import image1 from "../../assets/images/image1.jpg";
+import "./galleryStyle.scss";
+
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+import { Link } from "react-router-dom";
 
 const Gallery = () => {
-  return (
-    <Splide>
-      <SplideSlide>
-        <img style={{ width: "100%" }} src={image1} alt="" />
-      </SplideSlide>
-      <SplideSlide>
-        <img style={{ width: "100%" }} src={image1} alt="" />
-      </SplideSlide>
-      <SplideSlide>
-        <img style={{ width: "100%" }} src={image1} alt="" />
-      </SplideSlide>
-      <SplideSlide>
-        <img style={{ width: "100%" }} src={image1} alt="" />
-      </SplideSlide>
-      <SplideSlide>
-        <img style={{ width: "100%" }} src={image1} alt="" />
+  // console.log(useWindowDimensions());
+  return useWindowDimensions().width < 960 ? (
+    <Splide
+      options={{
+        type: "loop",
+        rewind: true,
+        waitForTransition: false,
+        perPage: 1,
+        gap: "0rem",
+        perMove: 1,
+        focus: "center",
+      }}
+    >
+      <SplideSlide className="soos">
+        <Link className="cardLink" to="#">
+          <div className="slideCardContainer">
+            <div className="servicesCard">
+              {" "}
+              <img className="galleryImageTop" src={image1} alt="" />
+              <div className="galleryCardTitle">Baumeister</div>
+              <div className="galleryCardText">
+                Wir koordinieren alle am Bau mitwirkenden Unternehmen
+              </div>
+            </div>
+          </div>
+        </Link>
       </SplideSlide>
     </Splide>
+  ) : (
+    <div>Swag</div>
   );
 };
 
