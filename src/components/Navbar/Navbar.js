72aeac147d7a00bd5fcea3c1efce0e0a7f0dc213
Jonathan98-dev <email.jonathannuel@gmail.com>
2021-05-31 18:38:54 +0200

    active link detection finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/JUMLogo-png.png";
 import "./navbar.scss";
 import Dropdown from "./Dropdown";
@@ -7,6 +7,44 @@ import Dropdown from "./Dropdown";
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const [dropdown, setDropdown] = useState(false);
+
+  //active link detection (not nice but it works)
+  let location = useLocation();
+  useEffect(() => {
+    let activeSite = location.pathname.toLowerCase();
+    if (activeSite.includes("services")) {
+      document.getElementById("dienste").style.color = "var(--mainColor)";
+      document.getElementById("dienste").style.fontWeight = "600";
+
+      document.getElementById("kontakt").style.color = "black";
+      document.getElementById("kontakt").style.fontWeight = "400";
+      document.getElementById("ueberuns").style.color = "black";
+      document.getElementById("ueberuns").style.fontWeight = "400";
+    } else if (activeSite.includes("kontakt")) {
+      document.getElementById("kontakt").style.color = "var(--mainColor)";
+      document.getElementById("kontakt").style.fontWeight = "600";
+
+      document.getElementById("dienste").style.color = "black";
+      document.getElementById("dienste").style.fontWeight = "400";
+      document.getElementById("ueberuns").style.color = "black";
+      document.getElementById("ueberuns").style.fontWeight = "400";
+    } else if (activeSite.includes("ueberuns")) {
+      document.getElementById("ueberuns").style.color = "var(--mainColor)";
+      document.getElementById("ueberuns").style.fontWeight = "600";
+
+      document.getElementById("dienste").style.color = "black";
+      document.getElementById("dienste").style.fontWeight = "400";
+      document.getElementById("kontakt").style.color = "black";
+      document.getElementById("kontakt").style.fontWeight = "400";
+    } else {
+      document.getElementById("ueberuns").style.color = "black";
+      document.getElementById("ueberuns").style.fontWeight = "400";
+      document.getElementById("dienste").style.color = "black";
+      document.getElementById("dienste").style.fontWeight = "400";
+      document.getElementById("kontakt").style.color = "black";
+      document.getElementById("kontakt").style.fontWeight = "400";
+    }
+  }, [location]);
 
   const handleClick = () => {
     setClick(!click);
@@ -40,7 +78,7 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-Logo">
+      <Link id="home" to="/" className="navbar-Logo">
         <img src={logo} alt="Logo" />
       </Link>
       <div className="menu-icon" onClick={handleClick}>
@@ -53,19 +91,29 @@ const Navbar = () => {
           onMouseLeave={onMouseLeave}
           onMouseEnter={onMouseEnter}
         >
-          <Link to="#" className="nav-links">
+          <Link id="dienste" to="#" className="nav-links">
             Dienste
           </Link>
           {dropdown && <Dropdown CloseMobileMenu={closeMobileMenu} />}
         </li>
         <li className="nav-item">
-          <Link to="/kontakt" className="nav-links" onClick={closeMobileMenu}>
+          <Link
+            id="kontakt"
+            to="/kontakt"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
             Kontakt
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link to="/ueberuns" className="nav-links" onClick={closeMobileMenu}>
+          <Link
+            id="ueberuns"
+            to="/ueberuns"
+            className="nav-links"
+            onClick={closeMobileMenu}
+          >
             Über uns
           </Link>
         </li>
